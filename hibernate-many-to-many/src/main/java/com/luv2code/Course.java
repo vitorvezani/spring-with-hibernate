8e58a60dcf285b5a77884541441e872550fb324d
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2018-06-23 13:45:46 -0300

    Adding n to n project

--- hibernate-many-to-many/src/main/java/com/luv2code/Course.java
+++ hibernate-many-to-many/src/main/java/com/luv2code/Course.java
@@ -1,65 +1,91 @@
 package com.luv2code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="course")
+@Table(name = "course")
 public class Course {
 
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  @Column(name="id")
-  private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
 
-  @Column(name="title")
-  private String title;
+	@Column(name = "title")
+	private String title;
 
-  @ManyToOne(cascade={ CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
-  @JoinColumn(name="instructor_id")
-  private Instructor instructor;
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinColumn(name = "instructor_id")
+	private Instructor instructor;
 
-  public Course() {
-  }
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH })
+	@JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+	private List<Student> students;
 
-  public Course(String title) {
-    this.title = title;
-  }
+	public Course() {
+	}
 
-  public int getId() {
-    return id;
-  }
+	public Course(String title) {
+		this.title = title;
+	}
 
-  public void setId(int id) {
-    this.id = id;
-  }
+	public int getId() {
+		return id;
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public Instructor getInstructor() {
-    return instructor;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public void setInstructor(Instructor instructor) {
-    this.instructor = instructor;
-  }
+	public Instructor getInstructor() {
+		return instructor;
+	}
 
-  @Override
-  public String toString() {
-    return "Course [id=" + id + ", title=" + title + "]";
-  }
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public void addStudent(Student student) {
+		if (students == null) {
+			students = new ArrayList<>();
+		}
+		students.add(student);
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + "]";
+	}
 
 }
