9ebfaf1ab29567acb04b4fa7c2fe85b4d312abc7
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2018-07-28 18:53:26 -0300

    Add @After support for dao layer

--- web-customer-tracker/src/com/luv2code/springdemo/aop/MyLogInAppAspect.java
+++ web-customer-tracker/src/com/luv2code/springdemo/aop/MyLogInAppAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aop;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -31,8 +32,13 @@ public class MyLogInAppAspect {
 			pointcut = "execution(* com.luv2code.springdemo.dao.CustomerDAO.getCustomers(..))",
 			returning = "result")
 	public void AfterReturningLogInApp(JoinPoint jp, List<Customer> result) {
+		System.out.println("[after-returning] Executing " + jp.getSignature() + "...");
+		System.out.println("[after-returning] Result is " + result);
+	}
+	
+	@After("execution(* com.luv2code.springdemo.dao.CustomerDAO.getCustomers(..))")
+	public void AfterLogInApp(JoinPoint jp) {
 		System.out.println("[after] Executing " + jp.getSignature() + "...");
-		System.out.println("[after] Result is " + result);
 	}
 	
 }
